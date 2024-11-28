89c27093ec24304f4b9cd6c43c1661b1333e7abf
hoangnh4204 <hoangnh4204@gmail.com>
2024-11-28 18:15:04 +0700

    feat: initialize backend project

---
+++ student-management-system-server/src/main/java/com/hit/studentmanagementsystemserver/StudentManagementSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.hit.studentmanagementsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemServerApplication.class, args);
+	}
+
+}
