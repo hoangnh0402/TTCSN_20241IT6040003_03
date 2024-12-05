771ceac73e7326b93e1d67d5bd3d4365f73bb363
hoangnh4204 <hoangnh4204@gmail.com>
2024-12-06 00:51:46 +0700

    feat: add APIs subject management

--- student-management-system-server/src/main/java/com/example/projectbase/constant/RoleConstant.java
+++ student-management-system-server/src/main/java/com/example/projectbase/constant/RoleConstant.java
@@ -2,5 +2,6 @@ package com.example.projectbase.constant;
 
 public class RoleConstant {
   public static final String ADMIN = "ROLE_ADMIN";
-  public static final String USER = "ROLE_USER";
+  public static final String STUDENT = "ROLE_STUDENT";
+  public static final String TEACHER = "ROLE_TEACHER";
 }
