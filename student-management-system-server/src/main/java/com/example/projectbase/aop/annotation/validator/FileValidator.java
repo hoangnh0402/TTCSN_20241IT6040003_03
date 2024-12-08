5287482332bf25e60e7eab18d0ad4fb7e60ac79a
hoangnh4204 <hoangnh4204@gmail.com>
2024-12-09 01:13:30 +0700

    fix: api getAllStudentsInClassroom

---
+++ student-management-system-server/src/main/java/com/example/projectbase/aop/annotation/validator/FileValidator.java
@@ -0,0 +1,34 @@
+package com.example.projectbase.aop.annotation.validator;
+
+
+import com.example.projectbase.aop.annotation.ValidFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+public class FileValidator implements ConstraintValidator<ValidFile, MultipartFile> {
+
+  @Override
+  public boolean isValid(MultipartFile file, ConstraintValidatorContext constraintValidatorContext) {
+    boolean result = true;
+    if(file != null) {
+      String contentType = file.getContentType();
+      if (!isSupportedContentType(Objects.requireNonNull(contentType))) {
+        result = false;
+      }
+    }
+    return result;
+  }
+
+  private boolean isSupportedContentType(String contentType) {
+    return contentType.equals("image/png")
+        || contentType.equals("image/jpg")
+        || contentType.equals("image/jpeg")
+        || contentType.equals("image/webp")
+        || contentType.equals("image/gif")
+        || contentType.equals("video/mp4");
+  }
+
+}
