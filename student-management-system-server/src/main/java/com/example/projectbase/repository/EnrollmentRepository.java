5dd490e290238c947e66ce86f37f0e23cae207dd
Nguyễn Đức Tùng <110727407+ductung3008@users.noreply.github.com>
2024-12-14 22:33:53 +0700

    fix: remove student from classroom

--- student-management-system-server/src/main/java/com/example/projectbase/repository/EnrollmentRepository.java
+++ student-management-system-server/src/main/java/com/example/projectbase/repository/EnrollmentRepository.java
@@ -4,6 +4,7 @@ import com.example.projectbase.domain.entity.Classroom;
 import com.example.projectbase.domain.entity.Enrollment;
 import com.example.projectbase.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,7 @@ import java.util.Optional;
 public interface EnrollmentRepository extends JpaRepository<Enrollment, String> {
     boolean existsByUserAndClassroom(User user, Classroom classroom);
     List<Enrollment> findAllByClassroomId(String classroomId);
-    Optional<Enrollment> findByUserAndClassroom(User user, Classroom classroom);
+
+    @Query("SELECT e FROM Enrollment e WHERE e.user.id = :userId AND e.classroom.id = :classroomId")
+    Optional<Enrollment> findByUserIdAndClassroomId(String userId, String classroomId);
 }
